c0051d748c2b58da2900ac2dfa7d0327e7fd47c5
beatricek222 <beatricek222@gmail.com>
2022-11-04 20:25:23 +0000

    delete photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,8 +7,16 @@ class PhotosController < ApplicationController
 
   def show
     url_id = params.fetch("path_id")
-    matching_photos = Photo.where({:id => url_id})
+    matching_photos = Photo.where({ :id => url_id })
     @photo = matching_photos[0]
     render({ :template => "photo_templates/show.html.erb" })
+  end
+
+  def delete
+    id = params.fetch("path_id")
+    photo = Photo.where({ :id => id })[0]
+    photo.destroy
+    #render({ :template => "photo_templates/delete.html.erb" })
+    redirect_to("/photos")
   end
 end
