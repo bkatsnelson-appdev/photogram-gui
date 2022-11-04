f1c615da52d8344be9719f07155b08fc8d4454bf
beatricek222 <beatricek222@gmail.com>
2022-11-04 20:13:36 +0000

    Finished read

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,14 @@
 class PhotosController < ApplicationController
   def index
-
     matching_photos = Photo.all
-    @list_of_photos = matching_photos.order({:created_at => :desc})
-    render({ :template => "photo_templates/index.html.erb"})
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
+    render({ :template => "photo_templates/index.html.erb" })
+  end
+
+  def show
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({:id => url_id})
+    @photo = matching_photos[0]
+    render({ :template => "photo_templates/show.html.erb" })
   end
 end
